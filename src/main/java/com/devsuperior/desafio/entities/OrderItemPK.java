45523f27f1d868c209764641d2660cc6a64ca94f
Carlos Teixeira <carloshteixeira01@hotmail.com>
2026-08-24 14:23:58 -0300

    Entidades e relacionamentos do domínio

---
+++ src/main/java/com/devsuperior/desafio/entities/OrderItemPK.java
@@ -0,0 +1,44 @@
+package com.devsuperior.desafio.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public OrderItemPK() {
+    }
+
+    public OrderItemPK(Order order, Product product) {
+        this.order = order;
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}

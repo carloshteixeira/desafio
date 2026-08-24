d711b789154b9b6c28798af04337b696e94759eb
Carlos Teixeira <carloshteixeira01@hotmail.com>
2026-08-24 13:59:48 -0300

    Cria entidades Product Category e Order

---
+++ src/main/java/com/devsuperior/desafio/entities/Order.java
@@ -0,0 +1,40 @@
+package com.devsuperior.desafio.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant moment;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment) {
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+}

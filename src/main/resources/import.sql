520ecdc609c9f5ad264c153ca46a4b6c12b80212
Carlos Teixeira <carloshteixeira01@hotmail.com>
2026-08-25 17:36:46 -0300

    Adiciona dados iniciais de produtos e categorias

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,20 @@
+INSERT INTO tb_category (name) VALUES ('Livros');
+INSERT INTO tb_category (name) VALUES ('Eletrônicos');
+INSERT INTO tb_category (name) VALUES ('Computadores');
+
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('The Lord of the Rings', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 90.50, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('Smart TV', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 2190.00, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('Macbook Pro', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1250.00, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg');
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('PC Gamer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1200.00, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg');
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('Rails for Dummies', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 100.99, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg');
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (5, 1);
+
 INSERT INTO tb_role (authority) VALUES ('ROLE_CLIENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
